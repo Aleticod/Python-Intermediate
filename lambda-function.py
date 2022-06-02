9bf9010db8395c7922ac9c8c3a105ca2a524f28d
harveydb <harveydb@debianharv>
2022-06-02 02:22:44 +0200

    In this commit I've used a autopip8 to formating the lambda-funtion.py file

--- lambda-function.py
+++ lambda-function.py
@@ -1,12 +1,12 @@
 def run():
-  pass
-  # This is the definition of lambda function
-  # This lambda function return if a word is palindromo
-  palindromo = lambda string: string == string[::-1]
-  if(palindromo('ana')):
-    print("This word is a palindromo")
-  else:
-    print("This wors isn't palindromo")
+    # This is the definition of lambda function
+    # This lambda function return if a word is palindromo
+    def palindromo(string): return string == string[::-1]
+    if(palindromo('ana')):
+        print("This word is a palindromo")
+    else:
+        print("This wors isn't palindromo")
+
 
 if __name__ == '__main__':
-  run()
+    run()
